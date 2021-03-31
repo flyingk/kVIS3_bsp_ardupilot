36db77891a3516839faf4a1e732441ca71c12be3
matt <anderson_rayner@hotmail.com>
2021-03-31 11:53:05 -0700

    On import, create events based on mode changes (mode name and reason for mode change)

--- ImportFilter/import_APM.m
+++ ImportFilter/import_APM.m
@@ -30,6 +30,7 @@ end
 % Constants
 RTD = 180.0/pi;
 t_start = inf;
+t_end = -inf;
 
 % Load file
 if file==0
@@ -221,6 +222,7 @@ for ii = 1:numel(data_stream_names) % change to a 1 later
             % Add data to fds
             if ~isempty(DAT)
                 t_start = min(t_start,DAT(1,1));
+                t_end   = max(t_end  ,DAT(end,1));
                 
                 % Add a new leaf to the tree
                 fds = kVIS_fdsAddTreeLeaf(fds, groupName, varNames, varNames, varUnits, varFrames, DAT, parentNode, false);
@@ -265,11 +267,70 @@ end
 [~,idx] = sort(fds.fdata(1,2:end));
 fds.fdata(:,2:end) = fds.fdata(:,idx+1);
 
-fds.timeOffset = t_start;
 %% Fix up the time so starts at t = 0
+fds.timeOffset = t_start; t_end = t_end - t_start;
 for ii = 2:numel(fds.fdata(1,:))
     fds.fdata{fds.fdataRows.data,ii}(:,1) = fds.fdata{fds.fdataRows.data,ii}(:,1) - fds.timeOffset;
 end
+
+%% Add events based on flight mode changes
+% We can use MODE.ModeNum to work this out
+modeTimes   = kVIS_fdsGetChannel(fds, 'MODE','Time');
+modeNumbers = kVIS_fdsGetChannel(fds, 'MODE','ModeNum');
+modeReasons = kVIS_fdsGetChannel(fds, 'MODE','Rsn');
+
+modeTimes(1) = 0; % Force the first mode time to 0
+
+% Combine short and same mode changes
+if numel(modeTimes > 1)
+    ii = 2;
+    while ii < numel(modeNumbers)
+        if modeNumbers(ii-1) == modeNumbers(ii)
+            % Remove entry as mode didn't change
+            modeNumbers(ii) = [];
+            modeTimes(ii)   = [];
+            modeReasons(ii) = [];
+        end
+        ii = ii+1;
+    end
+end
+
+% Add a marker for the mode at log's end
+modeTimes(end+1) = t_end;
+modeNumbers(end+1) = modeNumbers(end);
+modeReasons(end+1) = modeReasons(end);
+
+% Loop through modeTimes and store data
+eventNumber = 0;
+for ii = 1:numel(modeTimes)-1
+    % Get info
+    t_in  = modeTimes(ii);
+    t_out = modeTimes(ii+1);
+    
+    % Check if change was valid
+    if t_out-t_in > 1.0
+        
+        modeReason = modes_Reason(modeReasons(ii));
+        
+        % TODO:  Need to work out if using Copter/Plane/Rover etc
+        %        Defaulting to COPTER for now though
+        modeType = modes_ArduCopter(modeNumbers(ii));
+%         modeType = modes_ArduPlane(modeNumbers(ii));
+        
+        % Fill out eList
+        eventNumber = eventNumber+1;
+        eList(eventNumber).type = modeType;
+        eList(eventNumber).start= t_in;
+        eList(eventNumber).end  = t_out;
+        eList(eventNumber).description = modeReason;
+        eList(eventNumber).plotDef='';
+    end
+        
+end
+
+% Add eList to eventList
+fds.eventList = eList;
+
 
 %% All done!
 fprintf('\nImport took %.2f s\n',toc);
@@ -377,3 +438,123 @@ rho = P ./ (R.*T);
 
 return
 end
+
+function modeName = modes_ArduCopter(modeNumber)
+% From https://github.com/ArduPilot/ardupilot/blob/master/ArduCopter/mode.h#L14
+
+switch (modeNumber)
+    case 0; modeName = 'STABILIZE';
+    case 1; modeName = 'ACRO';
+    case 2; modeName = 'ALT_HOLD';
+    case 3; modeName = 'AUTO';
+    case 4; modeName = 'GUIDED';
+    case 5; modeName = 'LOITER';
+    case 6; modeName = 'RTL';
+    case 7; modeName = 'CIRCLE';
+    case 9; modeName = 'LAND';
+    case 11; modeName = 'DRIFT';
+    case 13; modeName = 'SPORT';
+    case 14; modeName = 'FLIP';
+    case 15; modeName = 'AUTOTUNE';
+    case 16; modeName = 'POSHOLD';
+    case 17; modeName = 'BRAKE';
+    case 18; modeName = 'THROW';
+    case 19; modeName = 'AVOID_ADSB';
+    case 20; modeName = 'GUIDED_NOGPS';
+    case 21; modeName = 'SMART_RTL';
+    case 22; modeName = 'FLOWHOLD';
+    case 23; modeName = 'FOLLOW';
+    case 24; modeName = 'ZIGZAG';
+    case 25; modeName = 'SYSTEMID';
+    case 26; modeName = 'AUTOROTATE';
+    otherwise; modeName = ['UNKNOWN_(',num2str(modeNumber),')'];
+end
+return
+end
+
+function modeName = modes_ArduPlane(modeNumber)
+% from https://github.com/ArduPilot/ardupilot/blob/master/ArduPlane/mode.h#L21
+
+switch (modeNumber)
+    case 0; modeName = 'MANUAL';
+    case 1; modeName = 'CIRCLE';
+    case 2; modeName = 'STABILIZE';
+    case 3; modeName = 'TRAINING';
+    case 4; modeName = 'ACRO';
+    case 5; modeName = 'FLY_BY_WIRE_A';
+    case 6; modeName = 'FLY_BY_WIRE_B';
+    case 7; modeName = 'CRUISE';
+    case 8; modeName = 'AUTOTUNE';
+    case 10; modeName = 'AUTO';
+    case 11; modeName = 'RTL';
+    case 12; modeName = 'LOITER';
+    case 13; modeName = 'TAKEOFF';
+    case 14; modeName = 'AVOID_ADSB';
+    case 15; modeName = 'GUIDED';
+    case 16; modeName = 'INITIALISING';
+    case 17; modeName = 'QSTABILIZE';
+    case 18; modeName = 'QHOVER';
+    case 19; modeName = 'QLOITER';
+    case 20; modeName = 'QLAND';
+    case 21; modeName = 'QRTL';
+    case 22; modeName = 'QAUTOTUNE';
+    case 23; modeName = 'QACRO';
+    case 24; modeName = 'THERMAL';
+    otherwise; modeName = ['UNKNOWN_(',num2str(modeNumber),')'];
+end
+return
+end
+
+function modeReason = modes_Reason(reasonNumber)
+% from https://github.com/ArduPilot/ardupilot/blob/master/libraries/AP_Vehicle/ModeReason.h
+
+switch (reasonNumber)
+    case 0; modeReason = 'UNKNOWN';
+    case 1; modeReason = 'RC_COMMAND';
+    case 2; modeReason = 'GCS_COMMAND';
+    case 3; modeReason = 'RADIO_FAILSAFE';
+    case 4; modeReason = 'BATTERY_FAILSAFE';
+    case 5; modeReason = 'GCS_FAILSAFE';
+    case 6; modeReason = 'EKF_FAILSAFE';
+    case 7; modeReason = 'GPS_GLITCH';
+    case 8; modeReason = 'MISSION_END';
+    case 9; modeReason = 'THROTTLE_LAND_ESCAPE';
+    case 10; modeReason = 'FENCE_BREACHED';
+    case 11; modeReason = 'TERRAIN_FAILSAFE';
+    case 12; modeReason = 'BRAKE_TIMEOUT';
+    case 13; modeReason = 'FLIP_COMPLETE';
+    case 14; modeReason = 'AVOIDANCE';
+    case 15; modeReason = 'AVOIDANCE_RECOVERY';
+    case 16; modeReason = 'THROW_COMPLETE';
+    case 17; modeReason = 'TERMINATE';
+    case 18; modeReason = 'TOY_MODE';
+    case 19; modeReason = 'CRASH_FAILSAFE';
+    case 20; modeReason = 'SOARING_FBW_B_WITH_MOTOR_RUNNING';
+    case 21; modeReason = 'SOARING_THERMAL_DETECTED';
+    case 22; modeReason = 'SOARING_THERMAL_ESTIMATE_DETERIORATED';
+    case 23; modeReason = 'VTOL_FAILED_TRANSITION';
+    case 24; modeReason = 'VTOL_FAILED_TAKEOFF';
+    case 25; modeReason = 'FAILSAFE';
+    case 26; modeReason = 'INITIALISED';
+    case 27; modeReason = 'SURFACE_COMPLETE';
+    case 28; modeReason = 'BAD_DEPTH';
+    case 29; modeReason = 'LEAK_FAILSAFE';
+    case 30; modeReason = 'SERVOTEST';
+    case 31; modeReason = 'STARTUP';
+    case 32; modeReason = 'SCRIPTING';
+    case 33; modeReason = 'UNAVAILABLE';
+    case 34; modeReason = 'AUTOROTATION_START';
+    case 35; modeReason = 'AUTOROTATION_BAILOUT';
+    case 36; modeReason = 'SOARING_ALT_TOO_HIGH';
+    case 37; modeReason = 'SOARING_ALT_TOO_LOW';
+    case 38; modeReason = 'SOARING_DRIFT_EXCEEDED';
+    case 39; modeReason = 'RTL_COMPLETE_SWITCHING_TO_VTOL_LAND_RTL';
+    case 40; modeReason = 'RTL_COMPLETE_SWITCHING_TO_FIXEDWING_AUTOLAND';
+    case 41; modeReason = 'MISSION_CMD';
+    case 42; modeReason = 'FRSKY_COMMAND';
+    otherwise; modeReason = 'UNKNOWN';
+end
+
+return
+end
+
